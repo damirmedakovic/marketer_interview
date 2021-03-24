dc6e2bb992ec52bf39304c2afe1b695b186575be
Damir Medakovic <damir0096@hotmail.com>
2021-03-24 23:27:32 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,189 @@
+
+
+
+
+
+known_intruder_1 = """--o-----o--
+---o---o---
+--ooooooo--
+-oo-ooo-oo-
+ooooooooooo
+o-ooooooo-o
+o-o-----o-o
+---oo-oo---
+""" 
+
+known_intruder_2 = """---oo---
+--oooo--
+-oooooo-
+oo-oo-oo
+oooooooo
+--o--o--
+-o-oo-o-
+o-o--o-o
+"""
+
+radar_sample = """----o--oo----o--ooo--ooo--o------o---oo-o----oo---o--o---------o----o------o-------------o--o--o--o-
+--o-o-----oooooooo-oooooo---o---o----o------ooo-o---o--o----o------o--o---ooo-----o--oo-o------o----
+--o--------oo-ooo-oo-oo-oo-----O------------ooooo-----oo----o------o---o--o--o-o-o------o----o-o-o--
+-------o--oooooo--o-oo-o--o-o-----oo--o-o-oo--o-oo-oo-o--------o-----o------o-ooooo---o--o--o-------
+------o---o-ooo-ooo----o-----oo-------o---oo-ooooo-o------o----o--------o-oo--ooo-oo-------------o-o
+-o--o-----o-o---o-ooooo-o-------oo---o---------o-----o-oo-----------oo----ooooooo-ooo-oo------------
+o-------------ooooo-o--o--o--o-------o--o-oo-oo-o-o-o----oo------------o--oooo--ooo-o----o-----o--o-
+--o-------------------------oo---------oo-o-o--ooo----oo----o--o--o----o--o-o-----o-o------o-o------
+-------------------o----------o------o--o------o--------o--------o--oo-o-----oo-oo---o--o---o-----oo
+----------o----------o---o--------------o--o----o--o-o------------oo------o--o-o---o-----o----------
+------o----o-o---o-----o-o---o-----oo-o--------o---------------------------------o-o-o--o-----------
+---------------o-------o-----o-------o-------------------o-----o---------o-o-------------o-------oo-
+-o--o-------------o-o-----o--o--o--oo-------------o----ooo----o-------------o----------oo----o---o-o
+-o--o-------------o----oo------o--o-------o--o-----o-----o----o-----o--o----o--oo-----------o-------
+-o-----oo-------o------o----o----------o--o----o-----o-----o-------o-----------o---o-o--oooooo-----o
+-o--------o-----o-----o---------oo----oo---o-o---------o---o--oooo-oo--o-------o------oo--oo--o-----
+------------o---------o---------o----oooo-------------oo-oo-----ooo-oo-----o-------o-oo-oooooooo---o
+----------------------o------------oooooooo---o-----o-------o--oooooo-o------------o-o-ooooooo-o----
+------------o------o---o---o-------oo-oo--o--o---------o--o-o-o-ooooo-o--------------oo-o----o-oo-o-
+---o-o----------oo-------oo----o----oooooooo-------o----o-o-o-o-----o-o-----o----------ooo-oo--o---o
+-o-o---------o-o---------------o--o--o--ooo---ooo-------o------oo-oo------------o--------o--o-o--o--
+-------oo---------------------------o-oo----------o------o-o-------o-----o----o-----o-oo-o-----o---o
+---o--------o-----o-------o-oo-----oo--oo-o----oo----------o--o---oo------oo----o-----o-------o-----
+---o--ooo-o---------o-o----o------------o---------o----o--o-------o----o--------o----------------oo-
+---o------o----------------o----o------o------o---oo-----------o-------------o----------oo---------o
+--oo---------------o--o------o---o-----o--o-------------o------o-------o-----o-----o----o------o--o-
+-o-------o----------o-o-o-------o-----o--o-o-----------o-oo-----------o------o---------o-----o-o----
+----------o----o-------o----o--o------o------------o---o---------------oo----o-----ooo--------------
+----o--------oo----o-o----o--o------ooo----o-oooo---o--o-oo--------o-oo-----o-o---o-o--o-----oo-----
+------o--------o-ooooo----o---o--o-----o---------------o-o-------o-----o----------------------------
+o-------oo----o--oooooo-o---o--o------oooo----------o-oo-------o---o----------o------oo-------------
+-o---o----------o--oo-oo-o---o-----o-o-----------------------oo--o------o------o--------------------
+-----oo-o-o-o---ooooooooo----o----o--------o--o---oo---o------------o----------o-o---o------o-o--oo-
+------o------o---ooo-o---------------------------o--o---o---o----o--o-------o-----o------o----o----o
+-------o----------ooo-o-----o----o---o--o-oo--o--o-o--o------o--o-oo---ooo------------------------o-
+-o-------o------o-o--ooo--o---o---oo-----o----o-------------o----o-ooo-o------o--o-o------o-o-------
+---oo--o---o-o---------o---o--------------o--o-----o-------o-----o--o---o-oo--------o----o----o-----
+o------o----oo-o-----------oo--o---o--------o-o------o-------o-o------o-oo---------o-----oo---------
+----o--o---o-o-----------o---o------------o-------o----o--o--o--o-o---------------o-----------------
+-------oo--o-o-----o-----o----o-o--o----------------------o-------o------o----oo----ooo---------o---
+o-----oo-------------------o--o-----o-----------o------o-------o----o-----------o----------------o--
+--o---o-------o------------o--------------------o----o--o-------------oo---o---------oo--------o----
+--o--------o---------o------------o------o-------o------------o-------o---o---------oooooX--X-------
+------o--------------o-o-o---------o---o-------o--o-----o-------o-o----------o-----oo-ooo----------o
+--o---------------o----o--oo-------------o---------o-------------------oo---------oo-o-ooo----------
+-o-----------o------ooo----o----------------ooo-----o--------o--o---o-----------o-o-oooooo--------oo
+-o---o-------o---o-oooo-----o-------------------o----oo-----------------o--o--------o--o------o--o--
+-------o---o------oooooo--o----ooo--o--------o-------o----------------------------oo-oo-o--o--------
+o--oo------o-----oo--o-oo------------oo--o------o--o-------------oo----o------------oooo-o------oo--
+-----o----------ooooooooo--------------oo--------------oo-----o-----o-o--o------o----------o----o---
+"""
+
+
+def tokenize_intruder_signal(intruder_signal): 
+
+    dimensions = get_signal_dimensions(intruder_signal)
+
+    rows = []
+    
+    for i in range(0, dimensions[0]):
+        rows.append()
+
+
+
+def hamming(signal_a, signal_b):
+
+    if len(signal_a) != len(signal_b):
+        return -1 
+
+    hamming_distance = 0
+    for i, j in zip(signal_a, signal_b): 
+        if i != j:
+            hamming_distance += 1 
+
+    return hamming_distance 
+
+
+
+def get_signal_dimensions(signal):
+
+    rows = 0 
+
+    for i in signal:
+        if ord(i) == 10: 
+            rows += 1
+
+    columns = (len(signal)-rows)/rows            
+
+
+    return (rows, columns) 
+
+
+def detect_intruder(radar_signal, known_intruder):
+
+
+    # Get signal dimensions
+
+    radar_signal_dimensions = get_signal_dimensions(radar_signal)
+
+    known_intruder_dimensions = get_signal_dimensions(known_intruder)
+
+
+    # The sliding window runs until it reaches the edge 
+
+    end_index = int(len(radar_signal) - (radar_signal_dimensions[1] * known_intruder_dimensions[0] + known_intruder_dimensions[0]) + (radar_signal_dimensions[1] - known_intruder_dimensions[1]))
+
+
+    for i in range(0, end_index): 
+
+        window = "a"
+
+
+
+
+
+
+detect_intruder(radar_sample, known_intruder_1)
+
+
+
+
+"""
+
+def detect_intruder(radar_signal, intruder):
+
+
+    window_length = len(intruder)
+
+    # Keep track of potential threat
+
+    _min = float("inf")
+    _min_start_index = int()
+
+    for i in range(0, len(radar_signal) - window_length):
+        
+        sliding_window = radar_signal[i:i + window_length]
+        
+
+        # Take the hamming distance of the 
+        distance = hamming(sliding_window, intruder)
+
+        if distance < _min:
+            _min = distance 
+            _min_start_index = i
+
+    
+    print("Min hamming distance: ", _min)
+
+"""
+
+
+
+
+
+
+#detect_intruder(radar_sample, known_intruder_2)
+ 
+
+
+
+
+
+    
+
